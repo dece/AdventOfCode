61ec46dcf36adf730f607c62f6201b6e0168e9e7
Dece <shgck@pistache.land>
2019-12-15 18:42:55 +0100

    Day 10: draw asteroid field

--- 2019/day10.py
+++ 2019/day10.py
@@ -3,6 +3,9 @@ import math
 import numpy as np
 
 
+DIM = 40
+
+
 def main():
     with open("day10.txt", "rt") as input_file:
         amap = [line.rstrip() for line in input_file.readlines()]
@@ -13,6 +16,7 @@ def main():
             if value == "#":
                 asteroids.append((x, y))
 
+    # Part 1
     best_vis = 0
     best_pos = None
     for pos in asteroids:
@@ -20,6 +24,20 @@ def main():
         if vis > best_vis:
             best_vis, best_pos = vis, pos
     print(f"Best visibility is {best_vis} at {best_pos}.")
+    draw(asteroids, best_pos)
+
+def draw(asteroids, station_pos):
+    for y in range(DIM):
+        print(str(y).zfill(2), end=" ")
+        for x in range(DIM):
+            if (x, y) == station_pos:
+                print("@", end="")
+            elif (x, y) in asteroids:
+                print("█", end="")
+            else:
+                print(" ", end="")
+        print()
+    print("  " + "0123456789"*4)
 
 def num_visible_asts(asts, ref):
     asts = asts.copy()
