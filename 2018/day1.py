521cef0e58239c51141f3dfafb5672cd6bd00f84
Dece <shgck@pistache.land>
2019-12-22 01:41:19 +0100

    2018 day 1 part 2

--- 2018/day1.py
+++ 2018/day1.py
@@ -1,3 +1,19 @@
 import sys
 
-print(sum(int(line.rstrip()) for line in sys.stdin.readlines()))
+changes = [int(line.rstrip()) for line in sys.stdin.readlines()]
+
+# Part 1
+print("Simple sum:", sum(changes))
+
+# Part 2
+freq = 0
+history = {0: True}
+found = False
+while not found:
+    for c in changes:
+        freq += c
+        if freq in history:
+            found = True
+            break
+        history[freq] = True
+print("First repeated:", freq)
