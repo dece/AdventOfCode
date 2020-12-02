958cc852f39896f894ef15cdf235a270a51490a9
dece <shgck@pistache.land>
2020-12-02 13:54:18 +0100

    Add new day script

---
+++ 2020/new.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+from datetime import date
+
+TEMPLATE = """
+def main():
+    with open("{}", "rt") as f:
+        lines = [line.rstrip() for line in f.readlines()]
+
+
+if __name__ == "__main__":
+    main()
+"""
+
+day = date.today().day
+with open(f"day{day}.py", "wt") as f:
+    f.write(TEMPLATE.format(f"day{day}.txt"))
+os.system(f"python ../fetch.py {day}")
